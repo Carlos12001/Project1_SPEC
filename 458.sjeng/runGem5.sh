1a0edea5eaf6693e0b9338e8cb0ec8760d64cef1
Carlos12001 <carlos.andres12001@gmail.com>
2024-06-10 18:13:10 -0600

    feat: change to atomic

--- 458.sjeng/runGem5.sh
+++ 458.sjeng/runGem5.sh
@@ -2,4 +2,4 @@
 export GEM5_DIR=/home/carlos/Documents/gem5/gem5
 export BENCHMARK=./src/benchmark
 export ARGUMENT=./data/test.txt
-time $GEM5_DIR/build/ARM/gem5.opt -d m5out_arm $GEM5_DIR/configs/deprecated/example/se.py -c $BENCHMARK -o $ARGUMENT -I 100000000 --cpu-type=TimingSimpleCPU --caches --l2cache --l1d_size=128kB --l1i_size=128kB --l2_size=1MB --l1d_assoc=2 --l1i_assoc=2 --l2_assoc=1 --cacheline_size=64
+time $GEM5_DIR/build/ARM/gem5.opt -d m5out_arm_trace $GEM5_DIR/configs/deprecated/example/se.py -c $BENCHMARK -o $ARGUMENT -I 100000000 --cpu-type=AtomicSimpleCPU --caches --l2cache --l1d_size=128kB --l1i_size=128kB --l2_size=1MB --l1d_assoc=2 --l1i_assoc=2 --l2_assoc=1 --cacheline_size=64
